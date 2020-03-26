034a88dccb1b68bd48803d8a533be590f84e38aa
German <germanc97@javerianacali.edu.co>
2020-03-25 23:17:40 -0500

    primera subida

---
+++ SocialJ/Registrarse.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SocialJ
+{
+    public partial class Registrarse : ContentPage
+    {
+        public Registrarse()
+        {
+            InitializeComponent();
+        }
+    }
+}
